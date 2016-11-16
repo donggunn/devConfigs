7bfa52bd68e277297499bebcce3f43b91bd572a9
Off Ric Arcila <ricartomojo@gmail.com>
2016-11-16 12:31:49 -0500

    Tmux init with 3 panels and 2 windows

--- Unity/.termric.sh
+++ Unity/.termric.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-gnome-terminal -e tmux
+tmux new-session -s Dev -d -n Servers
+tmux split-window -t Dev
+tmux split-window -t Dev
+tmux new-window -n Local
+tmux select-window -t Servers
+tmux a -t Dev
+tmux kill-session -t Dev
