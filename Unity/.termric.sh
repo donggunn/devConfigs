cd1169bfae075f5de39de67f993ce3b8104d51b8
Off Ric Arcila <ricartomojo@gmail.com>
2016-11-17 09:41:27 -0500

    Fixing script for tmux batch

--- Unity/.termric.sh
+++ Unity/.termric.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-tmux new-session -s Dev -d -n Servers
-tmux split-window -t Dev
-tmux split-window -t Dev
-tmux new-window -n Local
-tmux select-window -t Servers
-tmux a -t Dev
-tmux kill-session -t Dev
+
+SESSIONNAME="Dev"
+
+HASSES=$(tmux has-session -t $SESSIONNAME)
+if [ $? -ne 0 ] && [ -n $HASSES ] 
+then
+	tmux new-session -s $SESSIONNAME -d -n Servers
+	tmux split-window -t $SESSIONNAME
+	tmux split-window -t $SESSIONNAME
+	tmux new-window -n Local
+	tmux select-window -t Servers
+fi
+tmux a -t $SESSIONNAME
+tmux kill-session -t $SESSIONNAME
